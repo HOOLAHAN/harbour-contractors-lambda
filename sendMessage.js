167dd2fcf651428e85a3bf83376d9c75a6fd04f2
Iain <iain.hoolahan@gmail.com>
2023-07-18 15:11:48 +0100

    sendMessage update

--- sendMessage.js
+++ sendMessage.js
@@ -2,24 +2,36 @@ const sendMessage = require("./lib/sendMessage");
 
 module.exports.sendMessage = async (event) => {
   const req = JSON.parse(event.body);
- 
+
   const name = req.name;
   const email = req.email;
-  const message = req.message;
-  const data = await sendMessage(name, email, message);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        data: data,
-        input: event,
-        name: name,
-        email: email,
-        message: message
-      },
-      null,
-      2
-    ),
-  };
+  const text_message = req.text_message;
+  try {
+    await sendMessage(name, email, text_message);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Message sent successfully',
+          name: name,
+          email: email,
+          text_message: message
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: 'Failed to send message',
+          error: error.message
+        },
+        null,
+        2
+      ),
+    };
+  }
 };
