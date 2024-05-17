74b0a9969103822f022cdddf8a1c471de032117d
Iain <iain.hoolahan@gmail.com>
2024-05-17 14:41:28 +0100

    removed unused files and updated listProjectImages to use CloudFront distribution

--- listProjectImages.js
+++ listProjectImages.js
@@ -1,15 +1,11 @@
-// ./listProjectImages.js
-
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 const BUCKET_NAME = 'harbourcontractorsimages';
+const CLOUD_FRONT_URL = 'https://d8b2kltqfdpv.cloudfront.net';
 
 exports.handler = async (event) => {
-
-    // Extract the projectName from the path parameter
     const projectName = event.pathParameters.projectName;
-
     const prefix = `${projectName}/`;
 
     try {
@@ -20,21 +16,31 @@ exports.handler = async (event) => {
         const s3Response = await s3.listObjectsV2(s3Params).promise();
 
         const imageUrls = s3Response.Contents
-            .filter(content => !content.Key.endsWith('/')) // Filter out directories
+            .filter(content => !content.Key.endsWith('/'))
             .map(content => {
-                // Generating full URL for each image
-                return `https://${BUCKET_NAME}.s3.eu-west-2.amazonaws.com/${encodeURIComponent(content.Key)}`;
+                return `${CLOUD_FRONT_URL}/${encodeURIComponent(content.Key)}`;
             });
 
         return {
             statusCode: 200,
-            headers: {"Content-Type": "application/json"},
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,GET" 
+            },
             body: JSON.stringify(imageUrls),
         };
     } catch (error) {
         console.error('Error listing images:', error);
         return {
             statusCode: 500,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,GET" 
+            },
             body: JSON.stringify({ message: 'Failed to list project images', error: error.message }),
         };
     }
